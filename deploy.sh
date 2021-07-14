1a4da43f961200c4dffdf13d93257aad9106a119
1650243458@qq.com <1650243458@qq.com>
2021-07-14 16:00:13 +0800

    临时提交

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:qiongqiong9191/vuepress.git main:gh-pages
+git push -f git@github.com:qiongqiong9191/vuepress.git main
 
 cd -
